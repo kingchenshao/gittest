12cee880b189a6ac694fdd3869fc22ea66d8d9fc
chen <chen@qq.com>
2022-04-11 10:11:43 +0800

    hot

--- src/main/java/com/atguigu/test.java
+++ src/main/java/com/atguigu/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("就这!");
         System.out.println("jiuzhe");
+        System.out.println("wqeqwe");
     }
 }
